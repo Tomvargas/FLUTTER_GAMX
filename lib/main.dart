b93c22140b233aeba8b42d87dc244965d1a0dbf7
Tomas <tomvargasd@gmail.com>
2020-05-03 16:14:53 -0500

    practicing grids

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
 //---------HORA
 //import 'package:intl/intl.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  
  
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
+    
     return MaterialApp(
       title: 'Game Gallery',
       theme: ThemeData(
@@ -17,12 +20,14 @@ class MyApp extends StatelessWidget {
         brightness: Brightness.dark,
       ),
       home: MyHomePage(title: 'GAM X'),
+      
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
+  
 
   // This widget is the home page of your application. It is stateful, meaning
 
@@ -30,25 +35,27 @@ class MyHomePage extends StatefulWidget {
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
+  
+
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String txt;
 
-// Colors to use
+  String txt;
+  // Colors to use
   Color bg= const Color(0xFF14161b);
   Color searchbg= const Color(0xFF1f2539);
   Color searchtxt= const Color(0xFF414a67);
   Color hblue= const Color(0xFF0089bc);
   Color blue= const Color(0xFF006b93);
   Color ctxt= const Color(0xFFeaeaea);
-
-  void _btn() {
+    void _btn() {
     var cit;
   }
 
   @override
   Widget build(BuildContext context) {
+    
     //---------------------------------------------HORA
     //DateTime now = DateTime.now();
     //String hora = DateFormat('HH:mm').format(now);
@@ -56,6 +63,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     return Scaffold(
+      
       backgroundColor: bg,
       body: Row(
         children:<Widget>[ 
@@ -80,6 +88,18 @@ class _MyHomePageState extends State<MyHomePage> {
           */ //---------------------------------------------------------------------
         ]
       ),
+
+    /*
+        GridView.count(
+        crossAxisCount: 2,
+        children: List.generate(100, (index){
+        return Center(child: Text('Caja $index', style: Theme.of(context).textTheme.headline,),);
+          },
+        ),
+      ),
+      
+    */
+     
       floatingActionButton: FloatingActionButton(
         child:Icon(Icons.more_horiz),
         onPressed: _btn,
@@ -89,4 +109,5 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+  
 }
